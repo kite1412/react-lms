c6acfc1d9d8d8723a0c47afd252636a706c316c3
Tomy <tomyaryafiosa@gmail.com.com>
2025-03-02 14:12:01 +0700

    adjust overall LoginPage content

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import bgImage from "../assets/loginBackground.png";
 import logo from "../assets/logo.svg";
+import eye from "../assets/eye.svg";
+import eyeSlash from "../assets/eye-slash.svg";
 
 function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="text-white w-full max-h-screen h-full bg-[url('./assets/loginBackground.png')] bg-cover bg-center flex justify-end items-center">
-      <div className="login-container text-black w-100 h-160  p-10 flex justify-center bg-white mx-13">
-        <div className="flex flex-col items-center  w-xs justify-evenly h-[80%] ">
+      <div className="absolute inset-0 backdrop-blur-[2px] bg-black/30 w-full h-full"></div>
+      <div className="login-container text-black w-[460px] h-160  p-10 flex justify-center bg-white mx-13 relative">
+        <div className="flex flex-col items-center  w-xs justify-evenly h-[90%] ">
           <img
             src={logo}
             alt="logo"
@@ -13,11 +19,9 @@ function LoginPage() {
             className="h-[190px] w-[190px]"
           />
           <h2>Sign in Your Account</h2>
-          <ul className="w-full flex flex-col gap-2">
-            <li className="flex flex-col">
-              <label htmlFor="username" className="font-light">
-                Username
-              </label>
+          <ul className="w-full flex flex-col gap-3 font-light">
+            <li className="flex flex-col gap-1">
+              <label htmlFor="username">Username</label>
               <input
                 type="text"
                 name="username"
@@ -25,23 +29,30 @@ function LoginPage() {
                 className="bg-[#D9D9D9] outline-none rounded-md h-7 px-2"
               />
             </li>
-            <li className="flex flex-col ">
-              <label htmlFor="password" className="font-light">
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                className="bg-[#D9D9D9] outline-none rounded-md h-7 px-2"
-              />
+            <li className="flex flex-col gap-1 ">
+              <label htmlFor="password">Password</label>
+              <div className="w-full relative ">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  className="bg-[#D9D9D9] outline-none rounded-md h-7 px-2 w-full pr-7"
+                />
+                <img
+                  src={showPassword ? eye : eyeSlash}
+                  alt="hidden"
+                  className="w-[20px] absolute cursor-pointer top-1 right-1"
+                  draggable="false"
+                  onClick={() => setShowPassword(!showPassword)}
+                />
+              </div>
               <a href="#" className="text-black text-xs self-end font-light">
                 Forgot password?
               </a>
             </li>
           </ul>
 
-          <button className="bg-[#0A376E] text-white w-full py-1 rounded-xl">
+          <button className="bg-[#0A376E] text-white w-full py-1 rounded-4xl hover:cursor-pointer">
             Sign In
           </button>
         </div>
