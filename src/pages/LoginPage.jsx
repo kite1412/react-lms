82e76c36477cedc627484fceb00228e6936656ba
Natha Raditya <natharaditya6@gmail.com>
2025-03-02 15:15:32 +0700

    Adjust login container sizing & text field height

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import bgImage from "../assets/loginBackground.png";
 import logo from "../assets/logo.svg";
 import eye from "../assets/eye.svg";
 import eyeSlash from "../assets/eye-slash.svg";
@@ -11,7 +10,7 @@ function LoginPage() {
     <div className="text-white w-full max-h-screen h-full bg-[url('./assets/loginBackground.png')] bg-cover bg-center flex justify-end items-center">
       <div className="absolute inset-0 backdrop-blur-[2px] w-full h-full"></div>
       <div className="absolute inset-0 bg-gradient-to-br from-[#010101] to-[#3c3b3b] opacity-80 w-full h-full"></div>
-      <div className="login-container text-black w-1/3 h-160  p-10 flex justify-center bg-white mx-13 relative">
+      <div className="login-container text-black w-1/3 min-w-[360px] max-w-[500px] h-160 p-10 flex justify-center bg-white mx-13 relative">
         <div className="flex flex-col items-center  w-xs justify-evenly h-[90%] ">
           <img
             src={logo}
@@ -27,7 +26,7 @@ function LoginPage() {
                 type="text"
                 name="username"
                 id="username"
-                className="bg-[#D9D9D9] outline-none rounded-md h-7 px-2"
+                className="bg-[#D9D9D9] outline-none rounded-md h-10 px-2"
               />
             </li>
             <li className="flex flex-col gap-1 ">
@@ -37,12 +36,12 @@ function LoginPage() {
                   type={showPassword ? "text" : "password"}
                   name="password"
                   id="password"
-                  className="bg-[#D9D9D9] outline-none rounded-md h-7 px-2 w-full pr-7"
+                  className="bg-[#D9D9D9] outline-none rounded-md h-10 px-2 w-full pr-9"
                 />
                 <img
                   src={showPassword ? eye : eyeSlash}
                   alt="hidden"
-                  className="w-[20px] absolute cursor-pointer top-1 right-1"
+                  className="w-[20px] absolute cursor-pointer top-3 right-3"
                   draggable="false"
                   onClick={() => setShowPassword(!showPassword)}
                 />
@@ -53,7 +52,7 @@ function LoginPage() {
             </li>
           </ul>
 
-          <button className="bg-[#0A376E] text-white w-full py-1 rounded-4xl hover:cursor-pointer">
+          <button className="bg-[#0A376E] text-white w-full py-1 rounded-4xl hover:cursor-pointer mt-4">
             Sign In
           </button>
         </div>
