4d5ef6e141c7ae7525364f4681fa83057bfe8ad7
Natha Raditya <natharaditya6@gmail.com>
2025-03-03 11:28:36 +0700

    Add indicator on selected menu

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -6,10 +6,9 @@ import ChatIcon from "../assets/chat.svg?react";
 import DashboardIcon from "../assets/dashboard.svg?react";
 import { dashboard, communication, courses, schedule } from "../constants/menus";
 
-// TODO: add indicator for selected menu
 function SideNavBar({ currentMenu, onClick }) {
   return (
-    <div className="flex flex-col w-1/5 min-w-[240px] h-screen bg-midnightBlue gap-5 p-2" >
+    <div className="flex flex-col w-1/5 min-w-[240px] h-screen bg-midnightBlue gap-4" >
       <Logo />
       <div className="flex flex-col gap-5">
         <Menu 
@@ -45,7 +44,7 @@ export default SideNavBar;
 
 function Logo() {
   return (
-    <div className="flex gap-2 items-center">
+    <div className="flex gap-2 items-center p-2">
       <img
         src={logo}
         className="w-[60px] h-[60px]" 
@@ -61,15 +60,22 @@ function Menu({icon, name, selected, onClick}) {
   return (
     <button 
       className={`
-        flex gap-5 items-center ${selected ? "text-orange" : "text-white"}
+        flex gap-3 items-center ${selected ? "text-orange" : "text-white"}
         hover:cursor-pointer transition-colors duration-300
-        tint-[]
       `}
       onClick={onClick}
     >
-      {icon}
-      <div>
-        {name}
+      <div 
+        className={`
+          h-full w-[3px] rounded-tr-[4px] rounded-br-[4px]
+          ${selected ? "bg-orange" : "bg-midnightBlue"}
+          transition-colors duration-500
+      `} />
+      <div className="flex gap-3 pt-3 pb-3">
+        {icon}
+        <div>
+          {name}
+        </div>
       </div>
     </button>
   );
